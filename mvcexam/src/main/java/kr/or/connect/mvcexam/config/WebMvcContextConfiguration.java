5f5c69c6c06d73ca8838935dd78f76773e16b484
김성훈 <167969787+seongffm@users.noreply.github.com>
2024-11-01 21:26:41 +0900

    11/01

---
+++ mvcexam/src/main/java/kr/or/connect/mvcexam/config/WebMvcContextConfiguration.java
@@ -0,0 +1,49 @@
+package kr.or.connect.mvcexam.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = { "kr.or.connect.mvcexam.controller" })
+public class WebMvcContextConfiguration implements WebMvcConfigurer {
+	  public WebMvcContextConfiguration() {
+	        System.out.println("WebMvcContextConfiguration 생성자 호출");
+	    }
+	@Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		System.out.println("addResourceHandlers 호출");
+		registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/META-INF/resources/webjars/").setCachePeriod(31556926);
+        registry.addResourceHandler("/css/**").addResourceLocations("/css/").setCachePeriod(31556926);
+        registry.addResourceHandler("/img/**").addResourceLocations("/img/").setCachePeriod(31556926);
+        registry.addResourceHandler("/js/**").addResourceLocations("/js/").setCachePeriod(31556926);
+    }
+	 // default servlet handler를 사용하게 합니다.
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+    	System.out.println("configureDefaultServletHandling 호출");
+    	configurer.enable();
+    }
+   
+    @Override
+    public void addViewControllers(final ViewControllerRegistry registry) {
+    		System.out.println("addViewControllers가 호출됩니다. ");
+        registry.addViewController("/").setViewName("main");
+    }
+    
+    @Bean
+    public InternalResourceViewResolver getInternalResourceViewResolver() {
+    	 System.out.println("getInternalResourceViewResolver 호출");
+    	InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+}
